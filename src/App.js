2b3bb3d7e9f3d32e8d6c6510f96207cf50a4b69d
boysimon10 <simondiouf2710@gmail.com>
2023-12-30 12:38:07 +0000

    Working but no desgin and code comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        show: false,
+        timeElapsed: 0,
+        person: {
+        fullName: 'John Doe',
+        bio: 'Web Developer',
+        imgSrc: 'https://via.placeholder.com/150',
+        profession: 'Full Stack Developer'
+        }
+    };
+}
+
+componentDidMount() {
+        this.interval = setInterval(
+            () => this.setState({ timeElapsed: this.state.timeElapsed + 1 }),
+            1000
+    );
+}
+
+componentWillUnmount() {
+    clearInterval(this.interval);
+}
+
+toggleShow = () => {
+    this.setState({ show: !this.state.show });
+};
+
+render() {
+    return (
+        <div>
+        <button onClick={this.toggleShow}>
+            {this.state.show ? 'Hide' : 'Show'} Details
+        </button>
+        {this.state.show && (
+            <div>
+                <h1>{this.state.person.fullName}</h1>
+                <h2>{this.state.person.profession}</h2>
+                <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+                <p>{this.state.person.bio}</p>
+            </div>
+        )}
+        <p>Temps: {this.state.timeElapsed} secondes</p>
     </div>
-  );
+    );
+}
 }
 
 export default App;
